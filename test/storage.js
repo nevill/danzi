cf4940d2be4b14b9ec067f5fd6682073d4c843f4
Nevill <nevill.dutt@gmail.com>
2014-07-12 15:39:32 +0800

    Use nock to test with Aws storage, close #4

--- test/storage.js
+++ test/storage.js
@@ -1,8 +1,12 @@
 var assert = require('assert');
+var nock = require('nock');
+var path = require('path');
 var storage = require('../lib/storage');
 
 var uploadPath = __dirname + '/upload';
-var config = { path: uploadPath };
+var config = {
+  path: uploadPath
+};
 
 describe('Filesystem Storage', function() {
   var fsStorage = new storage.FileSystemStorage(uploadPath);
@@ -10,4 +14,44 @@ describe('Filesystem Storage', function() {
   it('should have a upload path', function() {
     assert.equal(fsStorage.uploadPath, uploadPath);
   });
+});
+
+describe('Aws S3 Storage', function() {
+  var options = {
+    key: 'a very random key',
+    secret: 'highly encrypted secert',
+    bucket: 'danzi-test',
+    'x-amz-acl': 'public-read'
+  };
+
+  var fixtureFile = path.join(__dirname, 'fixture', 'danzi.jpg');
+
+  var awsStorage = new storage.AwsStorage(options);
+
+  before(function() {
+    var scope = nock('https://s3.amazonaws.com', {
+      filteringScope: function(scope) {
+        return /^https:\/\/.*\.s3.amazonaws.com/.test(scope);
+      }
+    })
+      .filteringRequestBody(/.*/, '*')
+      .filteringPath(/.*/, '*')
+      .put('*', '*')
+      .reply(200);
+    nock.disableNetConnect();
+  });
+
+  after(function() {
+    nock.restore();
+  });
+
+  it('should save the file to S3', function(done) {
+    awsStorage.save({
+      name: 'danzi.jpg',
+      path: fixtureFile,
+      dest: '/images/danzi.jpg',
+      type: 'image/jpeg'
+    }, done);
+  });
+
 });
