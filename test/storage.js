f58612b34b8817277bd95fa3b2dfa7f21ed50206
Nevill <nevill.dutt@gmail.com>
2014-07-12 22:19:50 +0800

    Add test for FilesystemStorage#save

--- test/storage.js
+++ test/storage.js
@@ -1,9 +1,12 @@
 var assert = require('assert');
+var fs = require('fs');
 var nock = require('nock');
 var path = require('path');
 var storage = require('../lib/storage');
 
 var uploadPath = __dirname + '/upload';
+var fixtureFile = path.join(__dirname, 'fixture', 'danzi.jpg');
+
 var config = {
   path: uploadPath
 };
@@ -11,8 +14,28 @@ var config = {
 describe('Filesystem Storage', function() {
   var fsStorage = new storage.FileSystemStorage(uploadPath);
 
+  after(function(done) {
+    var exec = require('child_process').exec;
+    var filePath = path.join(uploadPath, '*');
+
+    exec('rm -rf ' + filePath, done);
+  });
+
   it('should have a upload path', function() {
     assert.equal(fsStorage.uploadPath, uploadPath);
+  });
+
+  it('should store a file on local filesystem', function(done) {
+    fsStorage.save({
+      name: 'danzi.jpg',
+      path: fixtureFile,
+      type: 'image/jpeg'
+    }, function(err, dest) {
+      fs.exists(dest, function(exists) {
+        assert.ok(exists);
+        done();
+      });
+    });
   });
 });
 
@@ -23,8 +46,6 @@ describe('Aws S3 Storage', function() {
     bucket: 'danzi-test',
     'x-amz-acl': 'public-read'
   };
-
-  var fixtureFile = path.join(__dirname, 'fixture', 'danzi.jpg');
 
   var awsStorage = new storage.AwsStorage(options);
 
